ceddb6c4c4309f4d3a619772695b20a2d31f8670
Mario Nageh <marionageh7@gmail.com>
2023-10-26 23:38:20 +0300

    Storge Count

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,6 +40,20 @@ class DBStorage:
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
+    def get(self, cls, id: str):
+        """
+        Get Object By Id
+        :param cls:
+        :param id:
+        :return:
+        """
+        if not classes[cls] or not cls:
+            return None
+        return self.__session.query(classes[cls]).filter_by(id=id).first()
+
+    def count(self, cls=None):
+        pass
+
     def all(self, cls=None):
         """query on the current database session"""
         new_dict = {}
