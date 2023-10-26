3ff7fe7c115c0928147c8d5ee7815d78ee8817c3
Mario Nageh <marionageh7@gmail.com>
2023-10-27 01:03:41 +0200

    Add Port & Debug To Test File

--- test_helberton/test_get_count.py
+++ test_helberton/test_get_count.py
@@ -3,9 +3,11 @@
 """
 from models import storage
 from models.state import State
+from models.place import Place
 
 print("All objects: {}".format(storage.count()))
-# print("State objects: {}".format(storage.count(State)))
+print("State objects: {}".format(storage.count(State)))
+print("Place objects: {}".format(storage.count(Place)))
 
-# first_state_id = list(storage.all(State).values())[0].id
-# print("First state: {}".format(storage.get(State, first_state_id)))
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
