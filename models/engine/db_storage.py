52801c295cd9ef903f7d92e9aa41e8843f9fe12e
Mario Nageh <marionageh7@gmail.com>
2023-10-27 01:03:41 +0200

    Add Port & Debug To Test File

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,7 @@
 """
 Contains the class DBStorage
 """
+import urllib.parse
 
 import models
 from models.amenity import Amenity
@@ -28,15 +29,19 @@ class DBStorage:
     def __init__(self):
         """Instantiate a DBStorage object"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        """For Handel Uri Errors"""
+        HBNB_MYSQL_PWD = urllib.parse.quote(getenv('HBNB_MYSQL_PWD'))
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB), echo=False)
+        HBNB_PORT = getenv('HBNB_MYSQL_PORT', 3306)
+        IS_DEBUG = (getenv('HBNB_DEBUG', 'False')) != 'False'
+        self.__engine = create_engine(f'mysql+mysqldb://'
+                                      f'{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}'
+                                      f'@{HBNB_MYSQL_HOST}:'
+                                      f'{HBNB_PORT}/{HBNB_MYSQL_DB}'
+                                      , echo=IS_DEBUG)
+
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
