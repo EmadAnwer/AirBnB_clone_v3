ed417ab0bf537e09cbb75d9064767040c854175f
Emad Anwer <emadanwer.official@gmail.com>
2023-10-29 10:29:55 +0200

    add CORS - allowing: /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """this module is the entry point to the REST API"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
