5acf056a7699755c8d572abc984b9b19aeb94f1a
Emad Anwer <emadanwer.official@gmail.com>
2023-10-30 15:32:45 +0200

    test db storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -69,27 +69,23 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-
-class TestFileStorage(unittest.TestCase):
-    """Test the FileStorage class"""
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_returns_dict(self):
-        """Test that all returns a dictionaty"""
-        self.assertIs(type(models.storage.all()), dict)
+    def test_get(self):
+        """Test that get returns the object based on the class name and ID"""
+        from models import storage as db_storage
+        name = "Californiaaaaaaaaaaaaaaaaa"
+        state = State(name="Californiaaaaaaaaaaaaaaaaa")
+        state.save()
+        state_id = state.id
+        state_name = state.name
+        get_state = db_storage.get(State, state_id)
+        self.assertEqual(state.name, name)
+        self.assertEqual(state_id, get_state.id)
+        self.assertEqual(state_name, get_state.name)
 
     def test_count(self):
         """Test that count returns the number of objects in storage"""
-        self.assertIs(type(models.storage.count()), int)
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_no_class(self):
-        """Test that all returns all rows when no class is passed"""
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_new(self):
-        """test that new adds an object to the database"""
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_save(self):
-        """Test that save properly saves objects to file.json"""
+        from models import storage as db_storage
+        storage = db_storage
+        count = storage.count()
+        self.assertEqual(type(count), int)
+        self.assertEqual(count, len(storage.all()))
